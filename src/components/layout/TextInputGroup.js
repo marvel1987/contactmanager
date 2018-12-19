76365c02cc315755ec6989f209ddbd3bb5daf1de
Mark Velos <webdesignfan@gmail.com>
2018-12-19 15:57:07 +0800

    Add files via upload
    - initial files uploaded

---
+++ src/components/layout/TextInputGroup.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+const TextInputGroup = ({
+  type,
+  name,
+  placeholder,
+  label,
+  value,
+  onChange,
+  error
+}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextInputGroup.defaultProps = {
+  type: "text"
+};
+
+TextInputGroup.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string
+};
+
+export default TextInputGroup;
